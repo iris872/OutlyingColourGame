b09e7e1c1c44c60cc746b08dec87587708d921bf
Flexiiiii <flexion360@wightman.ca>
2022-12-23 20:20:09 -0500

    Implemented logarithmic difficulty, i am less drunk now

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//reference "logarithmicDifficulty" for difficulty calculations
+
 document.addEventListener("DOMContentLoaded", function () {
   const squares = document.querySelectorAll(".square");
 
@@ -47,22 +49,26 @@ function beginNewRound(squares) {
 function colorChoice() {
   let currentScoreElement = document.querySelector(".current-score");
   let currentScore = parseInt(currentScoreElement.innerHTML.split(" ")[1]);
-  let currentOffset = parseFloat(Math.random() * 0.5) + (0.1 - (currentScore / 1000));
+  let logatrithmicDiffuculty = ((7 * Math.log(currentScore + 2) * 3) / 1000).toFixed(3);
+    console.log(logatrithmicDiffuculty)
+  // 7 is used to multiply the strength of the log, the +2 is to avoid -Infinity and 0, the 1000 brings it to a viable decimal point
+  // and the 4 is to make the values align with my already established values such as at 100 the offset of the color becomes 0.15 - 0.129* (very hard to see) *(currentScore = 100)
 
-  let r = Math.floor((Math.random() * 235) + 20);
-  let g = Math.floor((Math.random() * 235) + 20);
-  let b = Math.floor((Math.random() * 235) + 20);
-  let a = parseFloat((Math.random() * 0.6).toFixed(3)) + 0.2;
-
+  let r = Math.floor(Math.random() * 225 + 30);
+  let g = Math.floor(Math.random() * 225 + 30);
+  let b = Math.floor(Math.random() * 225 + 30);
+  let a = parseFloat(Math.random() * 0.6 + 0.2).toFixed(3); // this number should never exceed .8 and never be less than .2
+  let currentOffset = 0.15 - logatrithmicDiffuculty; // this number should never exceed .1 and should gradually decrease over time
   let correctColor = `rgba(${r}, ${g}, ${b}, ${a})`;
 
-  let outlyingAlpha = Math.random() * 2 === 1 ? parseFloat(a) - parseFloat(currentOffset) : parseFloat(a) + parseFloat(currentOffset);
-
-  outlyingAlpha > 1 ? outlyingAlpha = 1 : outlyingAlpha = outlyingAlpha;
+  let outlyingAlpha =
+    Math.random() * 2 === 1
+      ? parseFloat(a) - parseFloat(currentOffset)
+      : parseFloat(a) + parseFloat(currentOffset);
 
   let outlyingColor = `rgba(${r}, ${g}, ${b}, ${outlyingAlpha})`;
-  console.log("Correct Colour: " + outlyingColor)
-  console.log("Incorrect Colour " +correctColor)
+  console.log("Correct Colour: " + outlyingColor);
+  console.log("Incorrect Colour " + correctColor);
 
   return [correctColor, outlyingColor];
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+for (i = 0; i < 101; i++) {
+  let logatrithmicDiffuculty = 0.15 - ((7 * Math.log(i + 2) * 3) / 1000).toFixed(3);
+  console.log(logatrithmicDiffuculty);
+}
